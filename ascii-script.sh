c3e650f0b44604da463dd3dcce0abe2b434d8c63
patanna <56516638+patanna@users.noreply.github.com>
2024-11-21 15:36:41 +0100

    cat komanda

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y 
 cowsay -f dragon "Run over cover, I am DRAGON...RAWR" >> dragan.txt
-grep -i "dragon" dragon.txt
-ls -ltra 
+grep -i "dragon" dragan.txt
+ls -ltra
+cat dragan.txt 
